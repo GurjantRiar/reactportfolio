4b75eb24b1393348cde7b6a6219476dfc73d1968
Gurjant Riar <Gurjant@Amandeeps-MacBook-Pro.local>
2021-08-13 08:21:27 -0400

    uploads

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,8 +5,26 @@ const Home = () => {
     <div>
       <h1>Hi, This is Gurjant Singh Riar</h1>
       <h2>And I’m a Front End Engineer</h2>
-      <p class="footer">New Jersey  |     (484) 855-8660    |    GurjantSRiar@gmail.com<br/>
- <h5>Gurjant S Riar</h5></p>
+      <ul className="skills">
+        <h1>Skills I know</h1>
+        <li>
+          Front End ES6, ReactJS, PHP, JavaScript, HTML5, CSS3, Emotions JS,
+          WordPress, Drupal, Webpack, NPM, Yarn, Grunt
+        </li>
+        <li>Testing tools Chai, Mocha, Jest, Enzyme, React Testing Library</li>
+        <li>Agile Framework & Issue Tracking Scrum and Jira</li>
+        <li>Software Development Models Agile, Kanban, Waterfall</li>
+        <li>Source Control & Code Review GitHub</li>
+        <li>IDE’s VS Code, Brackets</li>
+        <li>
+          Familiar with continuous integration systems with the use of Jenkins
+        </li>
+      </ul>
+      <p class="footer">
+        New Jersey | (484) 855-8660 | GurjantSRiar@gmail.com
+        <br />
+        <h5>Gurjant S Riar</h5>
+      </p>
     </div>
   );
 };
